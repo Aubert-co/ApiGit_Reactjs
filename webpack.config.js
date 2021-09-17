643fbcdee6040b5f4ac5cb55a3876cbf70382e21
Black <53127242+Aubert-co@users.noreply.github.com>
2021-09-16 23:21:10 -0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HTMLWEBPACK = require('html-webpack-plugin')
+const webpack = require('webpack')
+module.exports = {
+    //soucemap mapeamento de todos os arquivos
+    devtool:'source-map',
+   // mode:'production',
+    entry :'./src/index.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundler.js'
+    },
+      devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port:'7000',
+        host:'localhost',
+        watchOptions: {
+            poll: true
+        }
+  },
+module:{
+    rules:[
+        {
+            test:/\.(js|jsx)$/,
+            exclude:/node_modules/,
+            use:{
+                loader:"babel-loader"
+            }
+        }
+    ]
+},
+plugins:[
+    new HTMLWEBPACK({
+        template:"./public/index.html",
+        filename:"./index.html"
+    }),
+    
+]
+}
+/**
+ * principais conceitos webpack
+ * Entry,output,loaders,pluginse mode
+ */
