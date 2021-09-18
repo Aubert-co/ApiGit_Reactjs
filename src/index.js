8c78560e4ae29a3c4c1de9867ace652af0983024
Black <53127242+Aubert-co@users.noreply.github.com>
2021-09-17 23:15:48 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-ReactDOM.render(<App />,document.getElementById('root'));
+import store from './store/index'
+import {Provider} from 'react-redux'
+ReactDOM.render(
+<Provider store={store}>
+<App />,
+</Provider>,
+document.getElementById('root')
+);
 
