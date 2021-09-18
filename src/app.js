0d29082457a1dfef9382eb4944a0c3b4072ff5c7
Black <53127242+Aubert-co@users.noreply.github.com>
2021-09-17 23:15:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import Header from './components/header'
+
 export default function App(){
-    return (<h1>Ola</h1>)
+
+    return (
+    <div className="container">
+        <Header/>
+
+        
+    </div>
+    )
+   
 }
