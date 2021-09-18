17c7a4007b39d44e578f60f1056114ac28e1f766
Black <53127242+Aubert-co@users.noreply.github.com>
2021-09-17 23:37:43 -0300

    Update search.js

--- src/components/search.js
+++ src/components/search.js
@@ -1,19 +1,27 @@
 import React,{useEffect, useState} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import  {findUsers,SelectDatas} from '../store/actionsSlice'
+import  {findUsers,findRepositories} from '../store/actionsSlice'
 
 function Search(){
     const [inputValue,SetInput] = useState('Search a user') 
-    const data = useSelector(state=>state.actions.data)
+    const data = useSelector(state=>state.actions.dataUsers)
     const dispatch  = useDispatch()
 
     const OnSearch = ()=>{
-        
+    try{
       fetch(`https://api.github.com/users/aubert-co`)
         .then((data)=>data.json())
-        .then((datas)=>{
-            dispatch(findUsers(datas))
+        .then((datas)=>dispatch(findUsers(datas)))
+        .finally(()=>{
+            fetch("https://api.github.com/users/Aubert-co/subscriptions")
+            .then((data)=>data.json())
+            .then((datas)=>{
+                dispatch(findRepositories(datas))
+            })
         })
+    }catch(err){
+        console.log('err')
+    }
     }
     const OnchangeInput = ({target})=>{
         SetInput(target.value)
