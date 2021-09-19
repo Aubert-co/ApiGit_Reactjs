c7f272de3afd6ab0220836cde738b33374222cca
Black <53127242+Aubert-co@users.noreply.github.com>
2021-09-18 22:14:06 -0300

    Update campos.js

--- src/components/campos.js
+++ src/components/campos.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 function renderDatas({name,login,public_repos,location,avatar_url},ind){
     return (
