792543ab626d2ba07a5ac941e61c00fe28ab44ac
Black <53127242+Aubert-co@users.noreply.github.com>
2021-09-19 21:07:18 -0300

    Create index.js

---
+++ src/services/index.js
@@ -0,0 +1,22 @@
+
+
+async function FindUsersService(Username){
+const datas = await fetch(`https://api.github.com/users/${Username}`)
+const values =  await datas.json()
+return values
+/*return fetch(`https://api.github.com/users/${Username}`)
+    .then((data)=>data.json())
+    .then((data)=>{
+       const {avatar_url,Login,name,public_repost,email,location} = data
+       //img.innerHTML = `<img src=${avatar_url}>`
+      //console.log(data)
+       return data
+})*/
+}
+
+async function FindSubscritionsService(subscriptions_url){
+    const datas = await fetch(subscriptions_url)
+    const jsonDatas = await datas.json()
+    return jsonDatas
+}
+export  {FindUsersService,FindSubscritionsService}
