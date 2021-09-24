a914387bc17c23248105bdf4294d751f9b79eba9
Black <53127242+Aubert-co@users.noreply.github.com>
2021-09-23 23:35:16 -0300

    Update repos.js

--- src/components/repos.js
+++ src/components/repos.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
+const map = (({repo,itens})=>{
 
+    return (
+        <div className="respoti">
+            <div className="name">{repo}</div>
+            <div className="itens">{itens}</div>
+        </div>
+    )
+})
 function RenderRepositories(){
 
     return (
